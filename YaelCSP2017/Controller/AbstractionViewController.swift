d70375d62a8557c177a19c8a7df0c3a4c9d9226e
Yael_Amaya <yiap2610@gmail.com>
2017-11-27 13:53:58 -0700

    lots of methods I don't understand for switching screens from class

--- YaelCSP2017/Controller/AbstractionViewController.swift
+++ YaelCSP2017/Controller/AbstractionViewController.swift
@@ -16,18 +16,99 @@ class AbstractionViewController: UIPageViewController, UIPageViewControllerDataS
         return [
             self.newAbstractionViewController(abstractionLevel: "Block"),
             self.newAbstractionViewController(abstractionLevel: "Java"),
-            self.newAbstractionViewController(abstractinLevel: "ByteCode"),
-            self.newAbstractionViewController(abstractonLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
             self.newAbstractionViewController(abstractionLevel: "AndGate")
         ]
     }()
     
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+    }
     
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0
+        else
+        {
+            return orderedAbstractionViews.last
+        }
+        
+        guard orderedAbstractionViews.count > previousIndex
+        else
+        {
+            return nil
+        }
+        
+        return orderedAbstractionViews[previousIndex]
+    }
     
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex >= 0
+        else
+        {
+            return nil
+        }
+        
+        guard nextIndex < orderedAbstractionViews.count
+        else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
     
+    //Mark:- Support fodts in the UIPageViewController
+    
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int
+    {
+        return orderedAbstractionViews.count
+    }
+    
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int
+    {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedAbstractionViews.index(of: firstViewController)
+        else
+        {
+            return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+    
+    //Mark:- Things below are things
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedAbstractionViews.first
+        {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
