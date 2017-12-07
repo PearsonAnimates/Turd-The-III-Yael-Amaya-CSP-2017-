89b4bbf66fbef4a161036df813f62cc0da68782e
Yael_Amaya <yiap2610@gmail.com>
2017-12-07 12:53:39 -0700

    Created custom cell render class

---
+++ YaelCSP2017/Controller/ArtCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCell.swift
+//  YaelCSP2017
+//
+//  Created by Amaya Penunuri, Yael on 12/7/17.
+//  Copyright © 2017 Amaya Penunuri, Yael. All rights reserved.
+//
+
+import UIKit
+
+class ArtCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+}
