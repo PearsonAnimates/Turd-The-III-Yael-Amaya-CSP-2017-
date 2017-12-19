2991924775cd6605910dab93efff498bfcab7c3a
Yael_Amaya <yiap2610@gmail.com>
2017-12-19 12:35:27 -0700

    Added a crapton of stuff for a space invaders minigame

---
+++ YaelCSP2017/Model/InvaderLaser.swift
@@ -0,0 +1,30 @@
+//
+//  InvaderLaser.swift
+//  Sample_CSP
+//
+//  Created by Cody Henrichsen on 12/7/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+public class InvaderBullet: Projectile
+{
+    override init(imageName: String, bulletSound:String?)
+    {
+        super.init(imageName: imageName, bulletSound: bulletSound)
+        
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.usesPreciseCollisionDetection = true
+        self.physicsBody?.categoryBitMask = CollisionCategories.InvaderBullet
+        self.physicsBody?.contactTestBitMask = CollisionCategories.Player
+        self.physicsBody?.collisionBitMask = 0x0
+    }
+    
+    required public init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+    
+}
---
+++ YaelCSP2017/Model/Player.swift
@@ -0,0 +1,103 @@
+//
+//  Player.swift
+//  Sample_CSP
+//
+//  Created by Cody Henrichsen on 12/7/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+public class Player: SKSpriteNode
+{
+    private var canFire : Bool = true
+
+    private var invincible = false
+    private var lives:Int = 3
+    {
+        didSet
+        {
+            if(lives < 0)
+            {
+                kill()
+            }
+            else
+            {
+                respawn()
+            }
+        }
+    }
+
+    
+    public init()
+    {
+        let texture = SKTexture(imageNamed: "x wing1")
+        super.init(texture: texture, color: SKColor.clear, size: texture.size())
+        
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.usesPreciseCollisionDetection = false
+        self.physicsBody?.categoryBitMask = CollisionCategories.Player
+        self.physicsBody?.contactTestBitMask = CollisionCategories.InvaderBullet | CollisionCategories.Invader
+        self.physicsBody?.collisionBitMask = CollisionCategories.EdgeBody
+        self.physicsBody?.allowsRotation = false
+        animate()
+    }
+    
+    required public init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+    }
+    
+    private func animate() -> Void
+    {
+        var playerTextures:[SKTexture] = []
+        for i in 1...3
+        {
+            playerTextures.append(SKTexture(imageNamed: "x wing\(i)"))
+        }
+        
+        let playerAnimation = SKAction.repeatForever(SKAction.animate(with: playerTextures, timePerFrame: 0.2))
+        self.run(playerAnimation)
+    }
+    
+    public func die () -> Void
+    {
+        
+    }
+    
+    public func kill() -> Void
+    {
+    
+    }
+    
+    public func respawn() -> Void
+    {
+        
+    }
+    
+    public func fireBullet(scene: SKScene) -> Void
+    {
+        if (!canFire)
+        {
+            return
+        }
+        else
+        {
+            canFire = false
+            let bullet = PlayerLaser(imageName: "Laser", bulletSound: "laser sound.mp3")
+            bullet.position.x = self.position.x
+            bullet.position.y = self.position.y + self.size.height / 2
+            scene.addChild(bullet)
+            let moveBulletAction = SKAction.move(to:CGPoint(x:self.position.x,y:scene.size.height + bullet.size.height), duration: 1.0)
+            let removeBulletAction = SKAction.removeFromParent()
+            bullet.run(SKAction.sequence([moveBulletAction, removeBulletAction]))
+            let waitToEnableFire = SKAction.wait(forDuration: 0.5)
+            run(waitToEnableFire,completion:
+                {
+                        self.canFire = true
+                })
+        }
+    }
+
+}
