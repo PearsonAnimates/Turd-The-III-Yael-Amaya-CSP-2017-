38227c426961ab95b54887f90bc14b9d2c05e0c8
Yael_Amaya <yiap2610@gmail.com>
2018-01-08 11:57:15 -0700

    Something or nothing!!! (I copied sentences that made no sense to me. D:)

---
+++ YaelCSP2017/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  YaelCSP2017
+//
+//  Created by Amaya Penunuri, Yael on 1/5/18.
+//  Copyright © 2018 Amaya Penunuri, Yael. All rights reserved.
+//
+
+import Foundation
